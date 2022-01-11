91b071a60f64709afe89fb6dd2b3d5e3d3eba4f0
ashin <ashin.jnv@gmail.com>
2022-01-11 12:47:41 +0530

    ccb

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -2,7 +2,7 @@ var app=angular.module('mainApp',[]);
 app.controller('people',function($scope,$http){
 $http({
 method:'GET',
-url:'/model/database.json'
+url:'./model/database.json'
 }).then(function(response){
     $scope.persons=response.data;
 });
