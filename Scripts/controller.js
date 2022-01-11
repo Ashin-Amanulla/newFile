c57d28d3177c3ec90fe1b5a976f5102d21d8ae0d
ashin <ashin.jnv@gmail.com>
2022-01-11 12:39:14 +0530

    cgxfc

---
+++ Scripts/controller.js
@@ -0,0 +1,9 @@
+var app=angular.module('mainApp',[]);
+app.controller('people',function($scope,$http){
+$http({
+method:'GET',
+url:'/model/database.json'
+}).then(function(response){
+    $scope.persons=response.data;
+});
+});
